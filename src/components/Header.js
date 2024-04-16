753c52e593b6155fb985247afd5484a4826ce581
Optimistique3452 <kartikeyasharma2006@gmail.com>
2024-04-16 21:27:14 +0530

    ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion"; // Import AnimatePresence for smooth image transition
 import Logo from "../assets/logowhite.png";
 
 const Header = () => {
@@ -15,35 +15,38 @@ const Header = () => {
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 3000); // Change image every 3 seconds
+    }, 7000); // Change image every 3 seconds
     return () => clearInterval(intervalId);
   }, []);
 
   return (
     <div className="w-screen h-screen relative" id="home">
-      {images.map((imageUrl, index) => (
-        <div
-          key={index}
-          className={`absolute inset-0 ${
-            index === currentImageIndex ? "block" : "hidden"
-          }`}
-          style={{ background: `url(${imageUrl})`, backgroundSize: "cover", backgroundAttachment: "fixed" }}
+      <AnimatePresence>
+        <motion.div
+          key={currentImageIndex}
+          className="absolute inset-0"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }} // For smooth exit transition
+          transition={{ duration: 1 }} // Adjust transition duration as needed
         >
-          <div className="absolute inset-0 bg-black opacity-80 object-cover"></div>
-          <motion.div className="absolute text-center inset-0 flex items-center flex-col gap-5 justify-center" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
-            <motion.img src={Logo} className="md:h-[180px] h-[120px]" alt="Logo" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, delay: 0.5 }} />
-            <motion.h1 className="text-white md:text-7xl font-bold uppercase text-4xl" initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, delay: 1 }}>
-              {texts[index]}
-            </motion.h1>
-            <motion.h1 className="text-gray-400 text-center text-lg md:text-xl w-[98%] mx-auto" initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, delay: 1.5 }}>
-              {subtexts[index]}
-            </motion.h1>
-            <a href="#about" className="button-77" role="button" initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, delay: 2 }}>
-              About Us
-            </a>
-          </motion.div>
-        </div>
-      ))}
+          <div className="absolute inset-0" style={{ backgroundImage: `url(${images[currentImageIndex]})`, backgroundSize: "cover", backgroundAttachment: "fixed" }}>
+            <div className="absolute inset-0 bg-black opacity-80 object-cover"></div>
+            <motion.div className="absolute text-center inset-0 flex items-center flex-col gap-5 justify-center" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
+              <motion.img src={Logo} className="md:h-[180px] h-[120px]" alt="Logo" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, delay: 0.5 }} />
+              <motion.h1 className="text-white md:text-7xl font-bold uppercase text-4xl" key={texts[currentImageIndex]} initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, delay: 1 }}>
+                {texts[currentImageIndex]}
+              </motion.h1>
+              <motion.h1 className="text-gray-400 text-center text-lg md:text-xl w-[98%] mx-auto" key={subtexts[currentImageIndex]} initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, delay: 1.5 }}>
+                {subtexts[currentImageIndex]}
+              </motion.h1>
+              <a href="#about" className="button-77" role="button" initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, delay: 2 }}>
+                About Us
+              </a>
+            </motion.div>
+          </div>
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 };
