bf346520c87efbf2735a4af8486bc03f862b847a
Optimistique3452 <kartikeyasharma2006@gmail.com>
2024-04-25 11:25:08 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./components/About";
 import Gallery from "./components/Gallery";
@@ -11,15 +12,76 @@ import Neer from "./components/Neer";
 import Samvesh from "./components/Samvesh";
 import Sahyog from "./components/Sahyog";
 import Moonj from "./components/Moonj";
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { useLocation } from "react-router-dom";
+
 function App() {
+  const [activeSection, setActiveSection] = useState("");
+  // const location = useLocation();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const homeElement = document.getElementById("home");
+      const aboutElement = document.getElementById("about");
+      const projectsElement = document.getElementById("projects");
+      const galleryElement = document.getElementById("gallery");
+      const teamElement = document.getElementById("team");
+      const contactElement = document.getElementById("contact");
+
+      const scrollPosition = window.scrollY + 150;
+
+      if (
+        scrollPosition < aboutElement.offsetTop ||
+        scrollPosition < homeElement.offsetTop
+      ) {
+        setActiveSection("home");
+      } else if (
+        scrollPosition >= aboutElement.offsetTop &&
+        scrollPosition < projectsElement.offsetTop
+      ) {
+        setActiveSection("about");
+      } else if (
+        scrollPosition >= projectsElement.offsetTop &&
+        scrollPosition < galleryElement.offsetTop
+      ) {
+        setActiveSection("projects");
+      } else if (
+        scrollPosition >= galleryElement.offsetTop &&
+        scrollPosition < teamElement.offsetTop
+      ) {
+        setActiveSection("gallery");
+      } else if (
+        scrollPosition >= teamElement.offsetTop &&
+        scrollPosition < contactElement.offsetTop
+      ) {
+        setActiveSection("team");
+      } else {
+        setActiveSection("contact");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup function to remove the scroll event listener
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    // Update the active section when the location changes
+    if (window.location.pathname === "/") {
+      setActiveSection("home");
+    }
+  }, []);
+
   return (
     <Router>
       <div className="App">
-      <Analytics/>
-      <SpeedInsights/>
-        <Navbar />
+        <Analytics />
+        <SpeedInsights />
+        <Navbar activeSection={activeSection} />
         <Routes>
           <Route
             path="/"
@@ -35,11 +97,11 @@ function App() {
               </div>
             }
           ></Route>
-          <Route path="/team" element={<TeamPage/>}/>
-          <Route path="/neer" element={<Neer/>}/>
-          <Route path="/samvesh" element={<Samvesh/>}/>
-          <Route path="/sahyog" element={<Sahyog/>}/>
-          <Route path="/moonj" element={<Moonj/>}/>
+          <Route path="/team" element={<TeamPage />} />
+          <Route path="/neer" element={<Neer />} />
+          <Route path="/samvesh" element={<Samvesh />} />
+          <Route path="/sahyog" element={<Sahyog />} />
+          <Route path="/moonj" element={<Moonj />} />
         </Routes>
       </div>
     </Router>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,11 @@ import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { FaBars } from "react-icons/fa";
 import enactus from "../assets/enactusNavbarLogo.png";
-import { useHistory, useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ activeSection }) => {
   const [showMenu, setShowMenu] = useState(false);
-  const [activeLink, setActiveLink] = useState("");
   const history = useNavigate();
-  const location = useLocation();
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
@@ -22,7 +20,7 @@ const Navbar = () => {
   };
 
   const handleHomeClick = () => {
-    if (location.pathname === "/") {
+    if (window.location.pathname === "/") {
       handleSmoothScroll("home");
     } else {
       history("/");
@@ -50,42 +48,42 @@ const Navbar = () => {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
           >
-            <a
-              onClick={() => { handleHomeClick("home"); setActiveLink("home"); }}
-              className={`text-gray-300 hover:bg-gray-600 transition-all duration-150 hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer ${activeLink === "home" ? "bg-gray-600" : ""}`}
-            >
-              Home
-            </a>
-            <a
-              onClick={() => { handleSmoothScroll("about"); setActiveLink("about"); }}
-              className={`text-gray-300 hover:bg-gray-600 transition-all duration-150 hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer ${activeLink === "about" ? "bg-gray-600" : ""}`}
-            >
-              About
-            </a>
-            <a
-              onClick={() => { handleSmoothScroll("projects"); setActiveLink("projects"); }}
-              className={`text-gray-300 hover:bg-gray-600 transition-all duration-150 hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer ${activeLink === "projects" ? "bg-gray-600" : ""}`}
-            >
-              Projects
-            </a>
-            <a
-              onClick={() => { handleSmoothScroll("gallery"); setActiveLink("gallery"); }}
-              className={`text-gray-300 hover:bg-gray-600 transition-all duration-150 hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer ${activeLink === "gallery" ? "bg-gray-600" : ""}`}
-            >
-              Gallery
-            </a>
-            <a
-              onClick={() => { handleSmoothScroll("team"); setActiveLink("team"); }}
-              className={`text-gray-300 hover:bg-gray-600 transition-all duration-150 hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer ${activeLink === "team" ? "bg-gray-600" : ""}`}
-            >
-              Team
-            </a>
-            <a
-              onClick={() => { handleSmoothScroll("contact"); setActiveLink("contact"); }}
-              className={`text-gray-300 hover:bg-gray-600 transition-all duration-150 hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer ${activeLink === "contact" ? "bg-gray-600" : ""}`}
-            >
-              Contact Us
-            </a>
+            <NavLink
+              label="Home"
+              id="home"
+              handleSmoothScroll={handleSmoothScroll}
+              active={activeSection === "home"}
+            />
+            <NavLink
+              label="About"
+              id="about"
+              handleSmoothScroll={handleSmoothScroll}
+              active={activeSection === "about"}
+            />
+            <NavLink
+              label="Projects"
+              id="projects"
+              handleSmoothScroll={handleSmoothScroll}
+              active={activeSection === "projects"}
+            />
+            <NavLink
+              label="Gallery"
+              id="gallery"
+              handleSmoothScroll={handleSmoothScroll}
+              active={activeSection === "gallery"}
+            />
+            <NavLink
+              label="Team"
+              id="team"
+              handleSmoothScroll={handleSmoothScroll}
+              active={activeSection === "team"}
+            />
+            <NavLink
+              label="Contact Us"
+              id="contact"
+              handleSmoothScroll={handleSmoothScroll}
+              active={activeSection === "contact"}
+            />
             <a
               href="https://enactus.org/"
               target="_blank"
@@ -96,42 +94,42 @@ const Navbar = () => {
             </a>
           </motion.div>
           <div className={`${showMenu ? "block" : "hidden"} flex items-center justify-center flex-col absolute top-[46px] right-[2rem] p-3 rounded-lg rounded-tr-none bg-gray-600 text-white`}>
-            <a
-              onClick={() => { handleHomeClick("home"); setActiveLink("home"); }}
-              className={`text-gray-300 hover:bg-gray-600 transition-all duration-150 hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer ${activeLink === "home" ? "bg-gray-600" : ""}`}
-            >
-              Home
-            </a>
-            <a
-              onClick={() => { handleSmoothScroll("about"); setActiveLink("about"); }}
-              className={`text-gray-300 hover:bg-gray-600 transition-all duration-150 hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer ${activeLink === "about" ? "bg-gray-600" : ""}`}
-            >
-              About
-            </a>
-            <a
-              onClick={() => { handleSmoothScroll("projects"); setActiveLink("projects"); }}
-              className={`text-gray-300 hover:bg-gray-600 transition-all duration-150 hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer ${activeLink === "projects" ? "bg-gray-600" : ""}`}
-            >
-              Projects
-            </a>
-            <a
-              onClick={() => { handleSmoothScroll("gallery"); setActiveLink("gallery"); }}
-              className={`text-gray-300 hover:bg-gray-600 transition-all duration-150 hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer ${activeLink === "gallery" ? "bg-gray-600" : ""}`}
-            >
-              Gallery
-            </a>
-            <a
-              onClick={() => { handleSmoothScroll("team"); setActiveLink("team"); }}
-              className={`text-gray-300 hover:bg-gray-600 transition-all duration-150 hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer ${activeLink === "team" ? "bg-gray-600" : ""}`}
-            >
-              Team
-            </a>
-            <a
-              onClick={() => { handleSmoothScroll("contact"); setActiveLink("contact"); }}
-              className={`text-gray-300 hover:bg-gray-600 transition-all duration-150 hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer ${activeLink === "contact" ? "bg-gray-600" : ""}`}
-            >
-              Contact Us
-            </a>
+            <NavLink
+              label="Home"
+              id="home"
+              handleSmoothScroll={handleSmoothScroll}
+              active={activeSection === "home"}
+            />
+            <NavLink
+              label="About"
+              id="about"
+              handleSmoothScroll={handleSmoothScroll}
+              active={activeSection === "about"}
+            />
+            <NavLink
+              label="Projects"
+              id="projects"
+              handleSmoothScroll={handleSmoothScroll}
+              active={activeSection === "projects"}
+            />
+            <NavLink
+              label="Gallery"
+              id="gallery"
+              handleSmoothScroll={handleSmoothScroll}
+              active={activeSection === "gallery"}
+            />
+            <NavLink
+              label="Team"
+              id="team"
+              handleSmoothScroll={handleSmoothScroll}
+              active={activeSection === "team"}
+            />
+            <NavLink
+              label="Contact Us"
+              id="contact"
+              handleSmoothScroll={handleSmoothScroll}
+              active={activeSection === "contact"}
+            />
             <a
               href="https://enactus.org/"
               target="_blank"
@@ -144,6 +142,17 @@ const Navbar = () => {
         </div>
       </div>
     </nav>
+  );
+};
+
+const NavLink = ({ label, id, handleSmoothScroll, active }) => {
+  return (
+    <a
+      onClick={() => handleSmoothScroll(id)}
+      className={`text-gray-300 hover:bg-gray-600 transition-all duration-150 hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer ${active ? "bg-gray-600" : ""}`}
+    >
+      {label}
+    </a>
   );
 };
 
